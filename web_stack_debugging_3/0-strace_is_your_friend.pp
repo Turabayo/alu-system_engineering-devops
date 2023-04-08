406587ebf39ebd08884a1c8fb020537ee3529586
Turabayo <i.turabayo@alustudent.com>
2023-04-08 11:31:03 -0700

    debugging3

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 #Puppet code
 
-exec { 'new-state':
-	command => 'sed -i s/phpp/php/g/var/www/html/wp-settings.php',
-	path => '/usr/local/bin/:/bin/'
+exec {'/var/www/html/wp-setting.php':
+	path => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+	command => "sed -i s/phpp/php/g/var/www/html/wp-settings.php",
+	
 }
