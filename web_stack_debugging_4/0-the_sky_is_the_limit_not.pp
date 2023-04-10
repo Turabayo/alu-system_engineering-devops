a9122273eb597c40eb13c14558d0416782ae5baa
Turabayo <i.turabayo@alustudent.com>
2023-04-10 10:46:31 -0700

    Website Debugging 4

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 #Increase traffic
 
-exec { 'ulimit':
-  command => 'sed -i s/15/2000/ /etc/default/nginx',
-  path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
+exec {'ulimit':
+  command => 'sed -i s/15/2000//etc/default/nginx',
+  path  => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
 } ->
 
-exec { 'restart':
-  command => 'sudo service nginx restart',
-  path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
+exec {'restart':
+ command => 'sudo service nginx restart',
+ path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
 }
